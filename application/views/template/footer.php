5c671633cc579318cddfd4e5204d993b1b26528c
Nigel M Peters <nutyga@gmail.com>
2015-11-25 18:07:40 +0000

    Updated template and added in new submodule

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,4 +1,25 @@
-			</section>									
+			</section>
+						
+
+			<?php if(isset($promos)): ?>
+
+			<!-- PROMOTION FEATURE ROW -->
+			<section class="container gallery">
+				<h3>Featured promotions</h3>
+
+					<?php $i = 0; foreach ($promos as $result): ?>
+						
+						<?php $desktop = ($result->promoid == 3 ? 'class="last-promo-desktop"' : false);  ?>			
+						<?php $tablet = ($result->promoid == 4 ? 'class="last-promo-tablet"' : false);  ?>			
+						<a <?php echo $desktop; echo $tablet; ?> href="<?php echo $result->promo_link; ?>" target="_blank">
+							<img src="<?php echo base_url('media').'/'.$result->filename.$result->ext; ?>" alt="<?php echo $result->promo_title; ?>">
+						</a>
+				
+					<?php $i++; endforeach; ?>
+
+			</section>			
+			<?php endif; ?>
+									
 	
 			<div class="push"></div>
 		</div>
@@ -33,6 +54,24 @@
         <script src="<?php echo $this->template_url; ?>js/vendor/jquery.cookie.js"></script>
         <script src="<?php echo $this->template_url; ?>js/vendor/jquery.cookiecuttr.js"></script>
         <link rel="stylesheet" href="<?php echo $this->template_url; ?>js/vendor/cookiecuttr.css" title="Cookie Cutter" type="text/css" media="screen" charset="utf-8">
+		<!—Westhill Analytics -->
+		<script type="text/javascript">
+		  var _paq = _paq || [];
+		  _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+		  _paq.push(["setCookieDomain", "*.openreach-connected.co.uk"]);
+		  _paq.push(["setDomains", ["*.openreach-connected.co.uk"]]);
+		  _paq.push(['trackPageView']);
+		  _paq.push(['enableLinkTracking']);
+		  (function() {
+		    var u="//analytics.westhill.co.uk/";
+		    _paq.push(['setTrackerUrl', u+'piwik.php']);
+		    _paq.push(['setSiteId', 35]);
+		    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+		    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+		  })();
+		</script>
+		<noscript><p><img src="//analytics.westhill.co.uk/piwik.php?idsite=35" style="border:0;" alt="" /></p></noscript>
+		<!-- End Westhill Analytics Code -->
 		<script>
 						
 		// You will need this for the menu toggle
